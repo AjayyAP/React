d39e5129a5baa340fb65ffca0337e10c5df06b4a
Ajayyap <ajayalakkasseryy@gmail.com>
2025-11-03 13:05:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Welcome from './embedding-expresiion/Welcome'
+// import NewBasic from './NewBasic'
+
+const App = () => {
+  return (
+    <div>
+      {/* <NewBasic/> */}
+      <Welcome/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/NewBasic.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewBasic = () => {
+  return (
+    <div>
+      <h2>welcome to react</h2>
+    </div>
+  )
+}
+
+export default NewBasic
