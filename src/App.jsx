62f724f1c42b478ae0c6cbf23eab3806ea1c1477
Ajayyap <ajayalakkasseryy@gmail.com>
2025-11-04 13:02:56 +0530

    file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
-import React from 'react'
-import Welcome from './embedding-expresiion/Welcome'
-// import NewBasic from './NewBasic'
+import './App.css'
+// import React from 'react'
+// // import Welcome from './embedding-expresiion/Welcome'
+// // import CallingFunction from './embedding-expresiion/CallingFunction'
+// // import Conditinal from './embedding-expresiion/Conditinal'
+// // import NewBasic from './NewBasic'
 
-const App = () => {
-  return (
-    <div>
-      {/* <NewBasic/> */}
-      <Welcome/>
-    </div>
-  )
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <NewBasic/> */}
+//       {/* <Welcome/> */}
+//       {/* <CallingFunction/> */}
+//       {/* <Conditinal/> */}
+
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+// class-components
+
+// import React, { Component } from 'react'
+// import BasicClass from './Class-Compnents/BasicClass'
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <BasicClass/>
+//       </div>
+//     )
+//   }
+// }
+import React, { Component } from 'react'
+import Counter from './embedding-expresiion/Conditinal'
+import Mounting from './Class-Compnents/life-cycle-method/Mounting'
+import Update from './Class-Compnents/life-cycle-method/Update'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <Counter/> */}
+        {/* <Mounting/> */}
+        <Update/>
+      </div>
+    )
+  }
 }
 
-export default App
+
+
+
